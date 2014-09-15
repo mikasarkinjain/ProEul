f6a5b027b47ebb5b586d2b1129f6b44837118e02
Mika Sarkin Jain <mikasarkinjain@gmail.com>
2014-09-15 18:08:35 -0400

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,9 +1,9 @@
+//requires fewer iterations and boolean statement evaluation than checking all numbers
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
 long sum = 0;
 long i, j;
-//following method requires fewer iterations (and fewer boolean statement evaluations)
 for (i = 1; i < 200; i++) if ((i*5)%3 != 0) sum += i*5;
 for (i = 1; i < 334; i++) sum += i*3;
 printf("ans: %ld\n", sum);
